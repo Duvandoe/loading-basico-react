6ee4cf79dd600684e962f65f36fca6d831dcf65c
Duvandoe <andresdoe30@gmail.com>
2025-01-15 17:23:27 -0500

    change

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ const Imagen = lazy(()=> import('./imagen'));
 function App() {
   return (
     <div className="App">
+      <div>
+        <h1>Imagenes</h1>
+      </div>
       <br/>
       <Suspense fallback={<h1>Cargando</h1>}>
         <Imagen />
